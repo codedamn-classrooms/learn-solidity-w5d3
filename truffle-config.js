ebb089de8dcea57b933e1aa1f2064fd48aef8ab9
Pranav <pranav.techiegeek@gmail.com>
2024-07-11 23:40:15 +0530

    fix: solidity versioning

--- truffle-config.js
+++ truffle-config.js
@@ -1,26 +1,26 @@
 module.exports = {
-  networks: {},
+	networks: {},
 
-  // Set default mocha options here, use special reporters etc.
-  mocha: {
-    useColors: false,
-    reporter: "mochawesome",
-    reporterOptions: {
-      reportFilename: process.env.UNIT_TEST_OUTPUT_FILE + ".json",
-      json: true,
-      html: false,
-    },
-  },
+	// Set default mocha options here, use special reporters etc.
+	mocha: {
+		useColors: false,
+		reporter: "mochawesome",
+		reporterOptions: {
+			reportFilename: process.env.UNIT_TEST_OUTPUT_FILE + ".json",
+			json: true,
+			html: false,
+		},
+	},
 
-  // Configure your compilers
-  compilers: {
-    solc: {
-      version: "native",
-      settings: {
-        optimizer: {
-          enabled: false,
-        },
-      },
-    },
-  },
+	// Configure your compilers
+	compilers: {
+		solc: {
+			version: "0.8.19",
+			settings: {
+				optimizer: {
+					enabled: false,
+				},
+			},
+		},
+	},
 };
